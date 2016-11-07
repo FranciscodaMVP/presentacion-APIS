ef0fc4108a35751941d6a6a062235625544ed1a3
FranciscodaMVP <isc.valdezfrancisco@gmail.com>
2016-11-06 23:01:10 -0800

    made changes to template and main

--- codeBlue/settings.py
+++ codeBlue/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'presentacion',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'codeBlue.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+#LOGIN_URL = reverse_lazy('start')
+#LOGIN_REDIRECT_URL = reverse_lazy('start')
+#LOGOUT_URL = reverse_lazy('login')
--- presentacion/views.py
+++ presentacion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
 # Create your views here.
+
+class Main_view(TemplateView):
+    template_name='main.html'

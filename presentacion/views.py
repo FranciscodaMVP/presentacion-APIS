28576de669779283539cffe4cf6aa35c2769f399
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-11-07 14:14:52 -0800

    Fix views

--- presentacion/views.py
+++ presentacion/views.py
@@ -3,7 +3,5 @@ from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 # Create your views here.
 
-<<<<<<< HEAD
 def index(request):
     return render(request, 'main.html')
->>>>>>> 2c6241b2a9e32b5f68b4ef7867de1465f7728b16

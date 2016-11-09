6a28347f42c5b48212ee618f19aba0c728b1d5d7
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-11-09 02:09:19 -0800

    Add models for authorization credentials and for Calendar example

--- presentacion/admin.py
+++ presentacion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import CredentialsModel, Event
 # Register your models here.
+
+admin.site.register(CredentialsModel)
+admin.site.register(Event)
--- presentacion/models.py
+++ presentacion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from oauth2client.contrib.django_util.models import CredentialsField
 
 # Create your models here.
+
+class CredentialsModel(models.Model):
+    user = models.OneToOneField(User)
+    credential = CredentialsField()
+
+class Event(models.Model):
+    summary = models.CharField(max_length=100)
+    date = models.DateField()
